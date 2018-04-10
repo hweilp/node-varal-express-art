127109e45d0c6fee1022194dd7f6c487872d25e6
hw <18379036722@163.com>
2018-04-10 10:42:35 +0800

    提交代码

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	port : 8888,
+	debug : true,
+	logPath : 'logs',
+	viewPath : 'views',
+	routesPath : 'routes',
+	staticPath : './public',
+	servicePath : 'services',
+	controllerPath : 'controllers'
+};
